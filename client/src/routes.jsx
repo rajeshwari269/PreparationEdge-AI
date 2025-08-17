8f0bb607ed443fba4dd4d236399f16c089b55f4e
Abhinav Mishra <149368945+CoderUzumaki@users.noreply.github.com>
2025-08-17 10:57:59 +0530

    Update routes.jsx

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,32 +1,3 @@
-// import { createBrowserRouter } from "react-router-dom";
-// import App from "./App";
-// import Login from "./pages/Login";
-// import SignUp from "./pages/SignUp"
-// import SetupForm from "./pages/CreateInterview";
-// import Interview from "./pages/Interview";
-// import InterviewReport from "./pages/Report";
-// import ProtectedRoute from "./components/ProtectedRoute";
-// import Dashboard from "./pages/Dashboard";
-// import Resources from "./pages/Resources";
-// import About from "./pages/About";
-// import Contact from "./pages/Contact";
-// import Error404 from "./pages/404";
-// import Footer from "./components/Footer";
-
-// const router = createBrowserRouter([
-//   { path: "/", element: <App /> },
-//   { path: "/login", element: <Login /> },
-//   { path: "/signup", element: <SignUp /> },
-//   { path: "/interview/setup", element: <ProtectedRoute><SetupForm /></ProtectedRoute> },
-//   { path: "/interview/:interviewId", element: <ProtectedRoute><Interview /></ProtectedRoute> },
-//   { path: "/interview/report/:interviewId", element: <ProtectedRoute><InterviewReport /></ProtectedRoute> },
-//   { path: "/dashboard", element: <ProtectedRoute><Dashboard /></ProtectedRoute> },
-//   { path: "/resources", element: <Resources /> },
-//   { path: "/about", element: <About /> },
-//   { path: "/contact", element: <Contact /> },
-//   { path: "*", element: <Error404 /> },
-// ]);
-
 // export default router;
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
@@ -41,7 +12,7 @@ import Dashboard from "./pages/Dashboard";
 import Resources from "./pages/Resources";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Error404 from "./pages/404";
+import Animated404 from "./pages/404";
 
 const router = createBrowserRouter([
   {
@@ -58,7 +29,7 @@ const router = createBrowserRouter([
       { path: "resources", element: <Resources /> },
       { path: "about", element: <About /> },
       { path: "contact", element: <Contact /> },
-      { path: "*", element: <Error404 /> },
+      { path: "*", element: <Animated404 /> },
     ],
   },
 ]);
