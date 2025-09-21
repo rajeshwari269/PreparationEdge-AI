f54332378f50b6b23619a46d921b4b8cfb6ba87e
Abhinav Mishra <149368945+CoderUzumaki@users.noreply.github.com>
2025-09-21 11:26:44 +0530

    Refactor CORS configuration in index.js
    Removed commented-out CORS configuration and simplified CORS setup.

--- server/index.js
+++ server/index.js
@@ -22,26 +22,12 @@ const upload = multer({ dest: "uploads/" });
 // app.use(cors({
 //   origin: 'https://prepedgeai.vercel.app',
 //           'http://localhost:3000' // Add others as needed
-//   credentials: true  //  Now correctly formatted as an object property
-// }));
+//   credentials: true
 
 app.use(cors({
   origin: true,
   credentials: true
 }));
-
-
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   credentials: true
-// }));
-
 
 app.use(express.json());
 connectDB()
