6268da41ac4325f0eed496328314516cf68c080b
Abhinav Mishra <149368945+CoderUzumaki@users.noreply.github.com>
2025-09-22 08:07:28 +0530

    Restore App component and related imports
    Uncommented the App component and its imports.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,3 @@
-// import Header from "./components/Header";
-// import Home from "./pages/Home";
-// import Footer from "./components/Footer";
-
-// function App() {
-// 	return (
-// 		<div className="min-h-screen bg-white">
-// 			<Header />
-// 			<Home />
-// 			<Footer />
-// 		</div>
-// 	);
-// }
-// export default App;
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
